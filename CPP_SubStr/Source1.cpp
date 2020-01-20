bd4ee341770d1ec3efd0a0978ae80e764bbe87ca
HopeDream <46891693+YoungWanHeeMOng@users.noreply.github.com>
2020-01-20 18:29:32 +0900

    F:\C++ Example\Examples\CPP_substr\subStr_CPP\CPP_substr\
    F:\C++ Example\Examples\CPP_substr\subStr_CPP\subStF:\C++ Example\Examples\CPP_substr\subStr_CPP\subStr_CPP\subStr_CPP.vcxprojr_CPP\subStr_CPP.vcxproj.filters

---
+++ CPP_SubStr/Source1.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string s = "ABCDE";
+	string temp = s.substr(0, 3);
+	cout << s << " " << temp << endl;
+
+
+	return 0;
+}
